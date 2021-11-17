cf09a9da5767c79d0d263cc18a71eb61e6babc79
Tobias Günther <Xyaren@users.noreply.github.com>
2021-11-17 14:55:38 +0100

    #126 replace `@ComponentScan` with `@Import`

--- extension/src/main/kotlin/CamundaRestClientSpringBootExtension.kt
+++ extension/src/main/kotlin/CamundaRestClientSpringBootExtension.kt
@@ -31,13 +31,16 @@ import feign.form.spring.SpringFormEncoder
 import feign.form.spring.SpringManyMultipartFilesWriter
 import feign.form.spring.SpringSingleMultipartFileWriter
 import org.camunda.bpm.extension.rest.config.CamundaRestClientProperties
+import org.camunda.bpm.extension.rest.impl.RemoteExternalTaskService
+import org.camunda.bpm.extension.rest.impl.RemoteRepositoryService
+import org.camunda.bpm.extension.rest.impl.RemoteRuntimeService
+import org.camunda.bpm.extension.rest.impl.RemoteTaskService
 import org.springframework.beans.factory.ObjectFactory
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.cloud.openfeign.support.SpringEncoder
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
@@ -46,7 +49,7 @@ import org.springframework.context.annotation.Import
  * Basic configuration of the extension.
  */
 @Configuration
-@ComponentScan
+@Import(RemoteExternalTaskService::class, RemoteRepositoryService::class, RemoteRuntimeService::class, RemoteTaskService::class)
 @EnableFeignClients
 @EnableConfigurationProperties(CamundaRestClientProperties::class)
 class CamundaRestClientSpringBootExtension {
