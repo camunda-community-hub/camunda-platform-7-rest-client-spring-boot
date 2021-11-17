f894d4ab53b1fa5f3c3b77dbf86461846888bb90
Tobias Günther <Xyaren@users.noreply.github.com>
2021-11-17 21:52:52 +0100

    #126 Add missing `@Import` for `RemoteHistoryService`

--- extension/src/main/kotlin/CamundaRestClientSpringBootExtension.kt
+++ extension/src/main/kotlin/CamundaRestClientSpringBootExtension.kt
@@ -31,16 +31,14 @@ import feign.form.spring.SpringManyMultipartFilesWriter
 import feign.form.spring.SpringSingleMultipartFileWriter
 import org.camunda.bpm.extension.rest.config.CamundaRestClientProperties
 import org.camunda.bpm.extension.rest.config.FeignErrorDecoderConfiguration
-import org.camunda.bpm.extension.rest.impl.RemoteExternalTaskService
-import org.camunda.bpm.extension.rest.impl.RemoteRepositoryService
-import org.camunda.bpm.extension.rest.impl.RemoteRuntimeService
-import org.camunda.bpm.extension.rest.impl.RemoteTaskService
+import org.camunda.bpm.extension.rest.impl.*
 import org.springframework.beans.factory.ObjectFactory
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.cloud.openfeign.support.SpringEncoder
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
@@ -51,6 +49,7 @@ import org.springframework.context.annotation.Import
 @Configuration
 @Import(
   RemoteExternalTaskService::class,
+  RemoteHistoryService::class,
   RemoteRepositoryService::class,
   RemoteRuntimeService::class,
   RemoteTaskService::class,
