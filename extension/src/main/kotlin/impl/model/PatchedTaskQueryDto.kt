179aabb2249ce4076b660d18a28140cf8190bc23
Jan Rohwer <jan.rohwer@holisticon.de>
2022-04-27 14:21:03 +0200

    fix: #177 fix typo and add test for taskIdIn

--- extension/src/main/kotlin/impl/model/PatchedTaskQueryDto.kt
+++ extension/src/main/kotlin/impl/model/PatchedTaskQueryDto.kt
@@ -6,7 +6,7 @@ import org.camunda.bpm.extension.rest.client.model.TaskQueryDto
 /**
  * Patched version of the generated TaskQueryDto as the OpenAPI spec from camunda is missing the fields taskId and taskIdIn.
  * Bug is reported to camunda https://jira.camunda.com/browse/CAM-14522 and fix will be included in future versions.
- * To allow these fields to be used in the meantime, this class provides the fields for usage in the POST request.git
+ * To allow these fields to be used in the meantime, this class provides the fields for usage in the POST request.
  */
 class PatchedTaskQueryDto : TaskQueryDto() {
 
